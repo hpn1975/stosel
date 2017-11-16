767506801fe800617906259471280b263cbe4081
hpn1975 <hans.nirmal@gmail.com>
2017-11-15 22:52:35 -0600

    save

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,45 +1,42 @@
-    $(document).on('ready', function () {
-        // initialization of carousel
-        $.HSCore.components.HSCarousel.init('.js-carousel');
+$(document).on('ready', function () {
+    // initialization of go to
+    $.HSCore.components.HSGoTo.init('.js-go-to');
 
-    // initialization of tabs
-    $.HSCore.components.HSTabs.init('[role="tablist"]');
+    // initialization of carousel
+    $.HSCore.components.HSCarousel.init('.js-carousel');
 
-      // initialization of popups
-      $.HSCore.components.HSPopup.init('.js-fancybox');
-
-      // initialization of go to
-      $.HSCore.components.HSGoTo.init('.js-go-to');
-
-      // initialization of text animation (typing)
-      $(".u-text-animation.u-text-animation--typing").typed({
-        strings: [
-          "an awesome template",
-          "perfect template",
-          "just like a boss"
-        ],
-        typeSpeed: 60,
-        loop: true,
-        backDelay: 1500
-      });
+    // initialization of HSDropdown component
+    $.HSCore.components.HSDropdown.init($('[data-dropdown-target]'), {
+        afterOpen: function () {
+            $(this).find('input[type="search"]').focus();
+        }
     });
 
-    $(window).on('load', function () {
-        // initialization of header
-        $.HSCore.components.HSHeader.init($('#js-header'));
+    // initialization of HSScrollBar component
+    $.HSCore.components.HSScrollBar.init($('.js-scrollbar'));
+
+    // initialization of masonry
+    $('.masonry-grid').imagesLoaded().then(function () {
+        $('.masonry-grid').masonry({
+            columnWidth: '.masonry-grid-sizer',
+            itemSelector: '.masonry-grid-item',
+            percentPosition: true
+        });
+    });
+
+    // initialization of popups
+    $.HSCore.components.HSPopup.init('.js-fancybox');
+});
+
+$(window).on('load', function () {
+    // initialization of header
+    $.HSCore.components.HSHeader.init($('#js-header'));
     $.HSCore.helpers.HSHamburgers.init('.hamburger');
 
-      // initialization of HSMegaMenu component
-      $('.js-mega-menu').HSMegaMenu({
+    // initialization of HSMegaMenu component
+    $('#dropdown-megamenu').HSMegaMenu({
         event: 'hover',
         pageContainer: $('.container'),
-        breakpoint: 991
-      });
+        breakpoint: 767
     });
-
-    $(window).on('resize', function () {
-        setTimeout(function () {
-            $.HSCore.components.HSTabs.init('[role="tablist"]');
-        }, 200);
-    });
-  
+});
